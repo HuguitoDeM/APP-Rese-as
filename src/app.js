e78781e793acf4add22fcb2e6cf801957dc85c77
HuguitoDeM <hugojavierdesideriomatinez@gmail.com>
2024-07-20 17:38:59 -0300

    update

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const cors = require("cors");
+const routerApi = require("./routes/index");
 
 dotenv.config();
 
