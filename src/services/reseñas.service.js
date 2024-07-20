4f66352d79f7681cd4c4d7328b538372a622b119
HuguitoDeM <hugojavierdesideriomatinez@gmail.com>
2024-07-20 17:17:14 -0300

    update

---
+++ src/services/reseñas.service.js
@@ -0,0 +1,19 @@
+const { models } = require("../libs/mongodb");
+
+class ReseñaService {
+  constructor() {}
+
+  async find() {
+    const res = await models.mongoose.find();
+    return res;
+  }
+
+  async create(data) {
+    const res = await models.mongoose.insertOne(data);
+    return res;
+  }
+  async delete_(id) {
+    const res = await models.mongoose.deleteOne(id);
+    return res;
+  }
+}
